6f1b8da10feb6e86ade58a13a7984448fe9c4ee4
arun <arun@securebroadcast.com>
2018-11-08 15:31:35 +0000

    forked django-oauth2-toolkit, making local changes to test user login

--- oauth2_provider/urls.py
+++ oauth2_provider/urls.py
@@ -8,7 +8,7 @@ app_name = "oauth2_provider"
 
 base_urlpatterns = [
     url(r"^authorize/$", views.AuthorizationView.as_view(), name="authorize"),
-    url(r"^token/$", views.TokenView.as_view(), name="token"),
+    url(r"^access_token/$", views.TokenView.as_view(), name="token"),
     url(r"^revoke_token/$", views.RevokeTokenView.as_view(), name="revoke-token"),
     url(r"^introspect/$", views.IntrospectTokenView.as_view(), name="introspect"),
 ]
